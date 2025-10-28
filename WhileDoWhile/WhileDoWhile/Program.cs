d207a5d6fb21f54a5ed60712b256647cf83f3515
Sevde Furuncu <furuncusvd@outlook.com>
2025-10-28 15:40:20 +0300

    While ve Do While döngüsü tamamlandı

---
+++ WhileDoWhile/WhileDoWhile/Program.cs
@@ -0,0 +1,26 @@
+﻿#region While Döngüsü
+//Console.Write("Kahveyi kim içiyor? : ");
+//string kahveIcen = Console.ReadLine();
+//Console.Write($"{kahveIcen} kahvesini kaç yudumda içiyor? : ");
+//int yudumSayisi = int.Parse(Console.ReadLine());
+//int yudumIndex = 0;
+//while (yudumSayisi > 0)
+//{
+//    Console.WriteLine($"{kahveIcen} kahvesinib {yudumIndex + 1} yudumda içiyor :)");
+//    Thread.Sleep(3000);
+//    yudumSayisi--;
+//    yudumIndex++;
+
+//}
+//Console.WriteLine($"{kahveIcen} kahvesini toplam {yudumIndex} yudumda içti.");
+#endregion
+
+#region Do While Döngüsü
+//int sayi = 1;
+//do
+//{
+//    Console.WriteLine($"Sayı:{sayi}");
+//    sayi++;
+//}
+//while (sayi <= 5);
+#endregion
