80bb4692268650cc49423a4824bacb1b2127c10a
Sevde Furuncu <furuncusvd@outlook.com>
2025-11-23 12:48:28 +0300

    js örnekleri yapıldı

--- DesignPatterns/DesignPatterns/Singleton/Logger.cs
+++ DesignPatterns/DesignPatterns/Singleton/Logger.cs
@@ -4,23 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//namespace DesignPatterns.Singleton
-//{
-//    public class Logger
-//    {
-//        private static Logger instance;
-//        private Logger() { }
-//        public static Logger GetInstance()
-//        {
-//            if (instance == null)
-//            {
-//                instance = new Logger();
-//            }
-//            return instance;
-//        }
-//        public void Log(string message)
-//        {
-//            Console.WriteLine($"Log----->{message}");
-//        }
-//    }
-//}
+namespace DesignPatterns.Singleton
+{
+    public class Logger
+    {
+        private static Logger instance;
+        private Logger() { }
+        public static Logger GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Logger();
+            }
+            return instance;
+        }
+        public void Log(string message)
+        {
+            Console.WriteLine($"Log----->{message}");
+        }
+    }
+}
