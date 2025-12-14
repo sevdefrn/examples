60c4c5f234c3fe7ae2932a596d980f76680fc73d
Sevde Furuncu <furuncusvd@outlook.com>
2025-12-14 14:29:22 +0300

    Mvc atibrust yapıldı

---
+++ MVC/Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models.ViewModels;
+
+namespace MVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(StudentViewModels model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
---
+++ MVC/Models/ViewModels/StudentViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Models.ViewModels
+{
+    public class StudentViewModels
+    {
+        [Required(ErrorMessage ="Bu alan boş geçilemez")]
+        [DisplayName("Ad Soyad")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="Bu alan boş geçilemez")]
+        [EmailAddress(ErrorMessage ="E-posta formatı doğru değil")]
+        [DisplayName("E-posta")]
+        public string Email {  get; set; }
+
+        [Range(18,60,ErrorMessage ="Yaş 18 ila 60 arasında olmalıdır")]
+        [DisplayName("Yaş")]
+        public int Age {  get; set; }
+    }
+}
