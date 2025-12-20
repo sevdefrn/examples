9ab3525e708b74c7b05e6a5b8fa5700d47982a0d
Sevde Furuncu <furuncusvd@outlook.com>
2025-12-20 17:51:14 +0300

    DÜZENLENDİ

--- Tekrar/Program.cs
+++ Tekrar/Program.cs
@@ -1,11 +1,72 @@
 ﻿#region 1.DERS TEKRAR
 #region HELLO WORLD
 
-Console.WriteLine("Hello World!");
-Console.WriteLine("Sevde Furuncu");
-Console.WriteLine("20.12.2025");
+//Console.WriteLine("Hello World!");
+//Console.WriteLine("Sevde Furuncu");
+//Console.WriteLine("20.12.2025");
+
+
+//Console.Write("Sevde" + " ");
+//Console.Write("Furuncu");
+
+//Console.Write("Lütfen adınız ve soyadınızı giriniz: ");
+//Console.WriteLine("Ekrana girilen değer: " + Console.ReadLine());
 
 #endregion
 
+#region DEĞİŞKENLER
+
+//string ad = "Sevde";
+//string soyad = "Furuncu";
+//Console.WriteLine(ad + " " + soyad);
+
+#region String Değişken
+
+//Console.Write("Lüten adınızı giriniz: ");
+//string ad = Console.ReadLine();
+//Console.Write("Lütfen soyadınız giriniz: ");
+//string soyad = Console.ReadLine();
+//Console.Write("Lütfen yaşınızı giriniz: ");
+//string yas = Console.ReadLine();
+//Console.WriteLine("Adınız:" + ad + " " + "Soyadınız:" + soyad + " " + "Yaşınız:" + yas);
 
 #endregion
+
+#endregion
+
+#endregion
+
+#region 2.Ders Tekrar
+
+#region int değişken
+
+//int sayi1 = 25;
+//int sayi2 = 5;
+//Console.WriteLine("İki sayını toplamı:" + (sayi1 + sayi2));
+//Console.WriteLine($"İki sayının çıkarması: {sayi1 - sayi2}");
+
+#region TOPLAMA
+int toplam = sayi1 + sayi2;
+#endregion
+
+#region ÇIKARMA
+int cıkarma = sayi1 - sayi2;
+#endregion
+
+#region ÇARPMA
+int carpma = sayi1 * sayi2;
+#endregion
+
+#region BÖLME
+int bolme = sayi1 / sayi2;
+#endregion
+
+//Console.WriteLine($"Sonuç:{toplam}");
+//Console.WriteLine($"Sonuç:{cıkarma}");
+//Console.WriteLine($"Sonuç:{carpma}");
+//Console.WriteLine($"Sonuç:{bolme}");
+
+#endregion
+
+#endregion
+
