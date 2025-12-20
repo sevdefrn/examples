e4ca3f9aa707569b00cada116b7dc4ee76b8233f
Sevde Furuncu <furuncusvd@outlook.com>
2025-12-20 17:31:29 +0300

    düzenşendi

---
+++ Tekrar/Program.cs
@@ -0,0 +1,11 @@
+﻿#region 1.DERS TEKRAR
+#region HELLO WORLD
+
+Console.WriteLine("Hello World!");
+Console.WriteLine("Sevde Furuncu");
+Console.WriteLine("20.12.2025");
+
+#endregion
+
+
+#endregion
