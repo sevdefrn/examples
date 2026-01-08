89935a816c753fd18cc9ecb8f01234db1ab4eca3
Sevde Furuncu <furuncusvd@outlook.com>
2026-01-08 20:21:03 +0300

    düzeltildi

--- DesignPatterns/DesignPatterns/Factory/Kare.cs
+++ DesignPatterns/DesignPatterns/Factory/Kare.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//namespace DesignPatterns.Factory
-//{
-//    public class Kare : ISekil
-//    {
-//        public void Ciz()
-//        {
-//            Console.WriteLine("----> Kare çizildi.");
-//        }
-//    }
-//}
+namespace DesignPatterns.Factory
+{
+    public class Kare : ISekil
+    {
+        public void Ciz()
+        {
+            Console.WriteLine("----> Kare çizildi.");
+        }
+    }
+}
--- DesignPatterns/DesignPatterns/Factory/SekilFactory.cs
+++ DesignPatterns/DesignPatterns/Factory/SekilFactory.cs
@@ -1,26 +1,26 @@
-﻿//using DesignPatterns.Singleton;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using DesignPatterns.Singleton;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace DesignPatterns.Factory
-//{
-//    public class SekilFactory
-//    {
-//        public static ISekil GetSekil(string name)
-//        {
-//            var logger = Logger.GetInstance();
-//            switch (name)
-//            {
-//                case "daire":
-//                    return new Daire();
-//                case "kare":
-//                    return new Kare();
-//                default:
-//                    throw new Exception("Bilinmeyen şekil.");
-//            }
-//        }
-//    }
-//}
+namespace DesignPatterns.Factory
+{
+    public class SekilFactory
+    {
+        public static ISekil GetSekil(string name)
+        {
+            var logger = Logger.GetInstance();
+            switch (name)
+            {
+                case "daire":
+                    return new Daire();
+                case "kare":
+                    return new Kare();
+                default:
+                    throw new Exception("Bilinmeyen şekil.");
+            }
+        }
+    }
+}
