0803ee154be2aaf67d6bdf24e730ec36060e2c60
Sevde Furuncu <furuncusvd@outlook.com>
2026-01-10 21:12:18 +0300

    Entity Framework giriş yapıldı

---
+++ EntityF/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityF
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }    
+        public decimal Price { get; set; }
+    }
+}
---
+++ EntityF/Program.cs
@@ -0,0 +1,49 @@
+﻿using EntityF;
+
+
+//var numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+#region LINQ 
+//var result = from q in numbers where q > 3 select q;
+//foreach(var number in result)
+//{
+//    Console.WriteLine(number);
+//}
+#endregion
+
+#region LAMBDA 
+
+//var lamdaResult = numbers.Where(x => x > 3).ToList();
+//lamdaResult.ForEach(x => Console.WriteLine(x));
+
+#endregion
+
+
+var products = new List<Product>
+{
+    new Product { ID = 1, Name = "Frambuazlı Pasta", Price = 1200 },
+    new Product { ID = 2, Name = "Yaban Mersinli Pasta", Price = 800 },
+    new Product { ID = 3, Name = "Yaban Mersinli Rolls", Price = 400 },
+    new Product { ID = 4, Name = "Profiterol", Price = 300 },
+    new Product { ID = 5, Name = "Sufle", Price = 300 },
+    new Product { ID = 6, Name = "Böğürtlenli Pasta", Price = 1200 },
+    new Product { ID = 7, Name = "Limonlu Cheesecake", Price = 600 },
+    new Product { ID = 8, Name = "Frambuazlı Cheesecake", Price = 600 },
+    new Product { ID = 9, Name = "Türk Kahvesi", Price = 250 }
+};
+
+
+#region LINQ 
+
+var urunler = from q in products where (q.Price > 1000) select q;
+foreach (var item in urunler)
+{
+    Console.WriteLine($"Linq ----> {item.Name}");
+}
+
+#endregion
+
+#region LAMBDA
+var lamdaurunler = products.Where(x => x.Price > 1000).ToList();
+lamdaurunler.ForEach(x => Console.WriteLine($"Lamda -----> {x.Name}"));
+#endregion
