9f580e96a84f021773969a068b37fa295e905d0a
Sevde Furuncu <furuncusvd@outlook.com>
2026-01-11 10:21:53 +0300

    Düzeltildi

--- EntityF/Program.cs
+++ EntityF/Program.cs
@@ -1,6 +1,5 @@
 ﻿using EntityF;
 
-
 //var numbers = new List<int> { 1, 2, 3, 4, 5 };
 
 #region LINQ 
@@ -18,20 +17,29 @@
 
 #endregion
 
-
 var products = new List<Product>
 {
     new Product { ID = 1, Name = "Frambuazlı Pasta", Price = 1200 },
-    new Product { ID = 2, Name = "Yaban Mersinli Pasta", Price = 800 },
+    new Product { ID = 2, Name = "Yaban Mersinli Pasta", Price = 1200 },
     new Product { ID = 3, Name = "Yaban Mersinli Rolls", Price = 400 },
     new Product { ID = 4, Name = "Profiterol", Price = 300 },
     new Product { ID = 5, Name = "Sufle", Price = 300 },
     new Product { ID = 6, Name = "Böğürtlenli Pasta", Price = 1200 },
     new Product { ID = 7, Name = "Limonlu Cheesecake", Price = 600 },
     new Product { ID = 8, Name = "Frambuazlı Cheesecake", Price = 600 },
-    new Product { ID = 9, Name = "Türk Kahvesi", Price = 250 }
+    new Product { ID = 9, Name = "Çikolata Parçalı Kurabiye", Price = 300 },
+    new Product { ID = 10, Name = "New York Cheesecake", Price = 600 },
+    new Product { ID = 11, Name = "Vanilyalı Cupcake", Price = 250 },
+    new Product { ID = 12, Name = "Beyaz Çikolatalı Sufle", Price = 300 },
+    new Product { ID = 13, Name = "Ekler Vanilya", Price = 600 },
+    new Product { ID = 14, Name = "Portakallı Kek", Price = 150 },
+    new Product { ID = 15, Name = "Tereyağlı Kurabiye", Price = 200 },
+    new Product { ID = 16, Name = "Makaron Çilek", Price = 600 },
+    new Product { ID = 17, Name = "Çikolatalı Cupcake", Price = 250 },
+    new Product { ID = 18, Name = "Bitter Çikolata", Price = 600 },
+    new Product { ID = 19, Name = "Sütlü Çikolata", Price = 600 },
+    new Product { ID = 20, Name = "Türk Kahvesi", Price = 250 }
 };
-
 
 #region LINQ 
 
